34f4c74130e544f14f51ddb98e6075fb09c3938b
ccoreil14 <ccoreil14@gmail.com>
2016-11-08 08:32:00 -0800

    a

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io  = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", function(socket){
+    console.log(socket.id + " user has connected"); 
+    socket.nickname = 'Guest';
+    socket.on("disconnect", function(){
+        console.log(socket.id + "user has disconnected");
+    });
+    
+    socket.on("chat message", function(msg){
+        console.log(msg.msg);
+        //console.log(io.sockets.clients());
+        io.emit('chat received', {
+            message: msg.msg,
+            sid: socket.id,
+            name: msg.name
+        });
+    });
+    
+    socket.on("get clients", function(msg){
+        console.log('sending clients');
+        console.log(io.sockets.clients());
+        io.emit('list clients', io.sockets.clients().adapter.sids);
+    });
+});
+
+app.use(express.static('public'));
+
+http.listen(3000, function() {
+    console.log("listening on port 3000");
+    
+})
