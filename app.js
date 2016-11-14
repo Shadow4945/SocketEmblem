49fd6e6145eaf2f58ebb0f8ac3d524a48b286ef0
bkinney33 <kinneybrennan33@gmail.com>
2016-11-14 08:48:35 -0700

    f

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-var express = require('express'),
-    app = express(),
-    http = require('http').Server(app),
-    io  = require('socket.io')(http);
-
-app.get('/', function(req, res) {
+var express = require('express')
+    , app = express()
+    , http = require('http').Server(app)
+    , io = require('socket.io')(http);
+app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
-
-io.on("connection", function(socket){
-    console.log(socket.id + " user has connected"); 
+var people = 0;
+io.on("connection", function (socket) {
+    console.log(socket.id + " user has connected");
     socket.nickname = 'Guest';
-    socket.on("disconnect", function(){
+    socket.on("disconnect", function () {
+
         console.log(socket.id + "user has disconnected");
     });
-    
-    socket.on("chat message", function(msg){
+    socket.on("chat message", function (msg) {
         console.log(msg.msg);
+        console.log(msg.name)
         //console.log(io.sockets.clients());
         io.emit('chat received', {
-            message: msg.msg,
-            sid: socket.id,
-            name: msg.name
+            message: msg.msg
+            , sid: socket.id
+            , name: msg.name,
+            score:msg.score,
+            y :msg.y,
+            x:msg.x,
         });
     });
-    
-    socket.on("get clients", function(msg){
-        console.log('sending clients');
-        console.log(io.sockets.clients());
+    socket.on("get clients", function (msg) {
         io.emit('list clients', io.sockets.clients().adapter.sids);
     });
 });
-
 app.use(express.static('public'));
-
-http.listen(3000, function() {
+http.listen(3000, function () {
     console.log("listening on port 3000");
-    
 })
--- public/script.js
+++ public/script.js
@@ -2,18 +2,21 @@ var stage;
 var my_name;
 
 $('document').ready(function () {
-    
+
     var socket = io();
     $('#client_info').submit(function (evt) {
         evt.preventDefault();
         var temp = '';
         socket.emit('get clients', temp);
     });
-    
+
     socket.on("list clients", function (data) {
-        console.log(data);
+          console.log(data)
+          player.x+=10;
+                player.y+=50;
+
     });
-    
+
     $('#message_form').hide();
     $('#name_form').submit(function (evt) {
         evt.preventDefault();
@@ -25,24 +28,30 @@ $('document').ready(function () {
 
     $('#message_form').submit(function (evt) {
         evt.preventDefault();
+          player.score+=100;
         var temp = {
             name: my_name,
-            msg: $('#msg').val()
+            msg: $('#msg').val(),
+            score:player.score,
+            y :Math.floor(player.y),
+            x:Math.floor(player.x)
         }
         socket.emit('chat message', temp);
         $('#msg').val("");
     });
 
     socket.on("chat received", function (data) {
-        $('#messages').prepend($('<li>').text(data.name + ' says: ' + data.message));
+
+        $('#messages').prepend($('<li>').text(data.name + ' says: ' + data.message+' also '+data.x+','+data.y+' Score:'+data.score));
     });
-    
+
     main();
 });
 
 var player = {
     x: ((Math.random()*60) * 10)+100,
-    y: 20
+    y: ((Math.random()*60) * 10)+100,
+    score:0
 }
 
 function setupCanvas() {
@@ -56,12 +65,11 @@ function createPlayer() {
     var rectangle = new createjs.Shape();
     rectangle.graphics.beginFill("#447").drawRect(0, 0, 20, 20);
     myText = new createjs.Text(my_name, "12px Arial", "#ffffff");  //creates text object
-    myText.x = 0; 
-    myText.y = 0; 
+    myText.x = 0;
+    myText.y = 0;
 }
 
 function main() {
     setupCanvas(); //sets up the canvas
     createPlayer();
 }
-
