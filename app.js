4c05f14bae9b393f69315319e9c31787d33a712b
Daniel Carapia <toonlink4943@gmail.com>
2016-12-01 08:41:13 -0700

    Game will keep track if someone closes their window or refreshes if people are in waiting room

--- app.js
+++ app.js
@@ -68,11 +68,15 @@ io.on("connection", function (socket) {
     socket.nickname = 'Guest';
     // io.to(socket.room).emit('updatePeopleInGame', peopleInGame);
     socket.on("disconnect", function () {
+        var isMainRoom = false;
         peopleInGame -= 1;
         clients.splice(clients.indexOf(socket.nickname), 1);
         console.log(clients.indexOf(socket.nickname) + " user has disconnected");
         //io.to(socket.room).emit('list clients', clients);
-        io.to(socket.room).emit('user left', socket.nickname);
+        if(socket.room === 'main room'){
+            isMainRoom = true;
+        }
+        io.to(socket.room).emit('user left', socket.nickname, isMainRoom);
         console.log(peopleInGame);
 
         //Try catch in case there are no people in the extra room
